19038b1ffca856af6953b20ce4a74795348b9535
Thibault Suzanne <thi.suzanne@gmail.com>
2014-09-19 17:13:38 +0200

    Adds onlyenv environement for beamer in org-mode

--- .emacs-custom.el
+++ .emacs-custom.el
@@ -4,6 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(inhibit-startup-screen t)
+ '(org-beamer-environments-extra (quote (("onlyenv" "O" "\\begin{onlyenv}%a" "\\end{onlyenv}"))))
  '(org-beamer-outline-frame-title "Plan")
  '(org-emphasis-regexp-components (quote ("     ('\"{" "-       .,:!?;'\")}\\" "," "." 1)) t)
  '(org-latex-listings (quote minted))
