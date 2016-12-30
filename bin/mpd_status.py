e64d6b4e8226ce1f771a9eeee3c022cd039f8714
Thibault Suzanne <thi.suzanne@gmail.com>
2016-12-30 20:28:00 +0100

    Add mpd_status.py and fix xmobar

---
+++ bin/mpd_status.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import mpd
+
+state_translate = {
+    "stop" : "Stopped",
+    "play" : "Playing",
+    "pause": "Paused",
+}
+
+client = mpd.MPDClient()
+client.connect("localhost", 6600)
+
+status = client.status()
+state = state_translate[status["state"]]
+
+if state == "Stopped":
+    print(state)
+else:
+    playlist = client.playlistinfo()
+    song = playlist[int(status["song"])]
+    title = song["title"]
+    artist = song["artist"]
+
+    print("%s: %s - %s" % (state, artist, title))
